b8c091c91601fa071b13b9677734824dcd2466a7
Chris <chris@ekahi.co>
2019-05-07 14:20:39 -1000

    More style work and added jQuery to add/remove additional input fields for images

--- public/js/app.js
+++ public/js/app.js
@@ -8,4 +8,22 @@ $(() => {
     $('#delete-btn').on('click', () => {
         $('#delete-form').submit();
     });
+    $('#form-add').on('click', () => {
+        const $container = $('#recipe-images');
+        $('<input>').attr(
+            {
+                type: 'text',
+                name: 'images[]'
+            }
+        ).addClass('form-control').appendTo($container);
+    });
+    $('#form-remove').on('click', () => {
+        const $container = $('#recipe-images');
+        const numOfFields = $container.children().length;
+        if (numOfFields < 2) {
+            return;
+        } else {
+            $($container.children().eq(numOfFields - 1)).remove();
+        }
+    });
 });
