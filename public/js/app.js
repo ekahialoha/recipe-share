440f705bc5bad75044765054f8a4557f9d025749
Chris <chris@ekahi.co>
2019-05-07 21:14:59 -1000

    add comments to javascript, carousel, styling for view

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,17 @@ $(() => {
     // =================
     // EVENT HANDLERS
     // =================
+    // GLOBAL used in conjunction with authentication
     $('#logout-link').on('click', () => {
         $('#logout-form').submit();
     });
+
+    // Used in view page in conjunction with authentication
     $('#delete-btn').on('click', () => {
         $('#delete-form').submit();
     });
+
+    // Used on create and edit pages. Allows dynamic image fields to appear and disappear
     $('#form-add').on('click', () => {
         const $container = $('#recipe-images');
         $('<input>').attr(
@@ -25,5 +30,32 @@ $(() => {
         } else {
             $($container.children().eq(numOfFields - 1)).remove();
         }
+    });
+
+    // Used on view page, will create carousel for recipes with multiple images
+    let currentImage = 0;
+    const $images = $('#images-block').children('img');
+    const lastImage = $images.length - 1;
+    $('#carousel-prev').on('click', () => {
+        console.log('prev');
+        // Hide current image
+        $images.eq(currentImage).hide();
+
+        // Determine previous image
+        currentImage = currentImage > 0 ? currentImage - 1 : lastImage;
+
+        // Show previous image
+        $images.eq(currentImage).show();
+    });
+    $('#carousel-next').on('click', () => {
+        console.log('next');
+        // Hide current image
+        $images.eq(currentImage).hide();
+
+        // Determine next image
+        currentImage = currentImage < lastImage ? currentImage + 1 : 0;
+
+        // Show the next image
+        $images.eq(currentImage).show();
     });
 });
